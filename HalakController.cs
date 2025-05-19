f826067b5b407ef4e3f03ae03096aa63f61977f8
RamirezLSS <130097227+RamirezLSS@users.noreply.github.com>
2025-05-19 18:03:14 +0200

    Add files via upload

---
+++ HalakController.cs
@@ -0,0 +1,95 @@
+﻿using HalakAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HalakAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HalakController : ControllerBase
+    {
+
+        [HttpDelete]
+        public IActionResult DeleteHalak(int id)
+        {
+            using (var context = new HalakContext())
+            {
+                try
+                {
+
+                    if (!context.Halaks.Select(t => t.Id).Contains(id))
+                    {
+
+                        return NotFound("Nincs ilyen ID-val termék");
+                    }
+                    else
+                    {
+                        Halak halak = new Halak() { Id = id };
+                        context.Halaks.Remove(halak);
+                        //context.SaveChanges();
+
+                        return Ok("Sikeres törlés");
+                    }
+                }
+
+                catch (Exception ex)
+                {
+                    return BadRequest(ex);
+                }
+            }
+        }
+
+        [HttpPut]
+        public IActionResult PutHalak(Halak halak)
+        {
+            using (var context = new HalakContext())
+            {
+                try
+                {
+
+                    if (!context.Halaks.Select(t => t.Id).Contains(halak.Id))
+                    {
+
+                        return NotFound("Nincs ilyen ID-val termék");
+                    }
+                    else
+                    {
+
+                        context.Halaks.Update(halak);
+                        //context.SaveChanges();
+
+                        return Ok("Sikeres módosítás");
+                    }
+                }
+
+                catch (Exception ex)
+                {
+                    return BadRequest(ex);
+                }
+            }
+        }
+
+        [HttpPost]
+        public IActionResult PostHalak(Halak halak)
+        {
+            using (var context = new HalakContext())
+            {
+                try
+                {
+
+                        context.Halaks.Add(halak);
+                        //context.SaveChanges();
+
+                        return Ok("Sikeres módosítás");
+                    
+                }
+
+                catch (Exception ex)
+                {
+                    return BadRequest(ex);
+                }
+            }
+        }
+
+    }
+}
---
+++ HorgaszokController.cs
@@ -0,0 +1,55 @@
+﻿using HalakAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HalakAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HorgaszokController : ControllerBase
+    {
+        [HttpGet("All")]
+        public IActionResult All()
+        {
+            using (var context = new HalakContext())
+            {
+                try
+                {
+                    var result = context.Halaks.ToList();
+                    return Ok(result);
+                }
+
+                catch (Exception ex)
+                {
+                    return BadRequest(ex);
+                }
+            }
+        }
+
+            [HttpGet("ById/{id}")]
+            public IActionResult AllById(int id)
+            {
+                using (var context = new HalakContext())
+                {
+                    try
+                    {
+
+                        var result = context.Halaks.Where(x => x.Id == id).Select(result => new
+                        {
+                            result.Id,
+                            result.MeretCm,
+                            result.Faj,
+                            result.Fogasoks,
+                            result.Kep
+                        }).ToList();
+                        return Ok(result);
+                    }
+
+                    catch (Exception ex)
+                    {
+                        return BadRequest(ex);
+                    }
+                }
+            }
+    } 
+}
