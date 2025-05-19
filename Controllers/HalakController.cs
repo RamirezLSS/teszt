103edd1ac796e211489e271179a1b8251d615bc5
RamirezLSS <130097227+RamirezLSS@users.noreply.github.com>
2025-05-19 17:59:52 +0200

    Add files via upload

---
+++ Controllers/HalakController.cs
@@ -0,0 +1,38 @@
+﻿using HalakAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HalakAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HalakController : ControllerBase
+    {
+        [HttpGet("ByHorgasz")]
+        public IActionResult GetByHorgasz(int id)
+        {
+            using (var context = new HalakContext())
+            {
+                try
+                {
+
+                    if (context.Horgaszoks.Select(h => h.Id).Contains(id))
+                    {
+                        var result = context.Fogasoks.Include(f => f.Horgasz).Include(f => f.Hal).Where(f => f.HorgaszId == id).Select(f => f.Hal).ToList();
+                        return Ok(result);
+                    }
+                    else
+                    {
+                        return NotFound("Nem létező horgász");
+                    }
+                }
+                catch (Exception ex)
+                {
+
+                    return BadRequest(ex.Message);
+                }
+            }
+        }
+    }
+}
